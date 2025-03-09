466764560534d2033f69f7085bf68995ec493e1d
ntimaks <ntimaks27@gmail.com>
2025-03-09 19:33:13 +0100

    added fake graph

--- components/chartCard.tsx
+++ components/chartCard.tsx
@@ -1,6 +1,6 @@
 export default function ChartCard() {
     return (
-        <div className="bg-white rounded-[8px] p-6 pb-8 w-[566px] h-[439px]">
+        <div className="rounded-[8px] bg-[#F7F7F7] w-full  ">
             <div className="flex justify-center">
                 <img src="/Graph.svg" alt="Chart" className="w-full h-auto" />
             </div>
