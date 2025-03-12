9313f65efc10dc6137a7e6622999d4813ab98135
ntimaks <ntimaks27@gmail.com>
2025-03-12 20:07:29 +0100

    tablet view

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center text-black bg-[#E6E6E6] 
       px-4 pt-8 pb-4 lg:p-[95px] gap-8 lg:gap-10 ">
 
-      <div className="flex flex-col lg:flex-row w-full gap-8 lg:gap-[32px]">
+      <div className="flex flex-col lg:flex-row w-full gap-8 lg:gap-[32px] items-center lg:items-start">
         <Suspense fallback={<div>Loading...</div>}>
           <LeftSection />
         </Suspense>
-        {/* Card  Section*/}
-        <div className="flex flex-col" style={{ width: '100%', maxWidth: '565.5px', gap: '16px' }}>
+        {/* Card Section*/}
+        <div className="flex flex-col mx-auto lg:mx-0" style={{ width: '100%', maxWidth: '565.5px', gap: '16px' }}>
           <ChartCard />
           <div className="flex flex-col lg:flex-row w-full lg:w-[565.5px] gap-4 lg:gap-[16px] lg:h-[274.5px]">
             <SwingImprovement />
