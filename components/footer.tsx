d3f843649374621e6fbca8ef4e75fe6f158a5d96
ntimaks <ntimaks27@gmail.com>
2025-03-12 18:55:23 +0100

    footer update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="h-16 flex items-center justify-center bg-black">
-            <p className="font-normal text-white">Copyright 2023 © HackMotion | All Rights Reserved</p>
+            <p className="font-normal text-white text-xs">Copyright 2023 © HackMotion | All Rights Reserved</p>
         </footer>
     );
 }
